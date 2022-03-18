b5b3dd96d4be8d2b7b42648861263ea4bda6ccc5
jbarker <jackson.barker@du.edu>
2022-03-17 19:35:58 -0600

    project update

--- src/data.js
+++ src/data.js
@@ -3,30 +3,30 @@ export const projects = [
     {
         title: "Eat the Frog",
         subtitle: "Task Manager",
-        description: "test test",
+        description: "",
         image: "https://github.com/Jackson-Barker/Portfolio-Jackson-Barker/blob/main/assets/images/EatThefrogSS.png?raw=true",
         link: "https://msp4msps.github.io/eat_the_frog/"
     },
     {
         title: "Trip Share",
         subtitle: "Travel Blog",
-        description: "test test",
+        description: "",
         image: "https://github.com/Jackson-Barker/portfolio/blob/main/src/images/screencapture-blooming-ridge-32748-herokuapp-2021-12-23-20_03_11.png?raw=true",
         link: "https://blooming-ridge-32748.herokuapp.com/"
     },
     {
         title: "Work Day Scheduler",
-        subtitle: "Day planning app",
-        description: "test test",
+        subtitle: "Day Planning App",
+        description: "",
         image: "https://github.com/Jackson-Barker/Work-Day-Scheduler/blob/main/assets/work-daySS.png?raw=true",
         link: "https://jackson-barker.github.io/Work-Day-Scheduler/"
     },
     {
-        title: "Password Generator",
-        subtitle: "Secure password generator",
-        description: "test test",
-        image: "https://github.com/Jackson-Barker/Password-Generator-Jackson-Barker/blob/main/assets/password-generatorSS.png?raw=true",
-        link: "https://jackson-barker.github.io/Password-Generator-Jackson-Barker/"
+        title: "Pad Protection",
+        subtitle: "Item Inventory App",
+        description: "",
+        image: "https://user-images.githubusercontent.com/88734760/155451129-5ec85667-b31c-4430-a4d9-02825ef6faf7.png",
+        link: "https://padprotection.herokuapp.com/"
     },
     
 
